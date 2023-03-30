3d725401a2cffff8dc5894b24bbea817004e3e64
XiovV <xiovv@tutanota.com>
2023-03-30 13:49:02 +0200

    imported CryptoService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { CryptoService } from 'src/crypto/crypto.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, JwtService],
+  providers: [UsersService, AuthService, JwtService, CryptoService],
   exports: [UsersService],
 })
 export class UsersModule {}

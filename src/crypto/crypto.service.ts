6b833728aeb0c1081871e9a8a95a13bc857d272a
XiovV <xiovv@tutanota.com>
2023-03-30 13:47:04 +0200

    added crypto package with helper methods for encrypting and decrypting the mfa secret

---
+++ src/crypto/crypto.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createCipheriv, createDecipheriv, randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
+import * as aes256 from 'aes256';
+
+@Injectable()
+export class CryptoService {
+    password: string;
+
+    constructor(config: ConfigService) {
+        this.password = config.get('AES_KEY');
+    }
+
+    encryptMfaSecret(secret: string) {
+        const encryptedSecret = aes256.encrypt(this.password, Buffer.from(secret))
+
+        return encryptedSecret;
+    }
+
+    decryptMfaSecret(secret: Buffer): Promise<string> {
+        const decryptedBuffer = aes256.decrypt(this.password, secret)
+
+        return decryptedBuffer.toString();
+    }
+}

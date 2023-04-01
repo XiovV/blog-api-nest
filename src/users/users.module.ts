f69b08f19893e9b5c2960a78c686f2f0fd45b03e
XiovV <xiovv@tutanota.com>
2023-04-01 15:00:10 +0200

    injected BlacklistedToken

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,9 +6,10 @@ import { User } from './entities/user.entity';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { CryptoService } from 'src/crypto/crypto.service';
+import { BlacklistedToken } from './entities/token-blacklist.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, BlacklistedToken])],
   controllers: [UsersController],
   providers: [UsersService, AuthService, JwtService, CryptoService],
   exports: [UsersService],

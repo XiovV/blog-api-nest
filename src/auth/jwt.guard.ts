3152e757dcf7e99e658a2d5bd60631f72efceea3
XiovV <xiovv@tutanota.com>
2023-04-01 14:58:21 +0200

    modified the guard so it doesn't verify the expiration date of the access token

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { JwtService } from "@nestjs/jwt";
+import { JwtService, JwtVerifyOptions } from "@nestjs/jwt";
 import { Request } from "express";
 import { UsersService } from "src/users/users.service";
 
@@ -9,15 +9,20 @@ export class JwtGuard implements CanActivate {
     constructor(private jwtService: JwtService, private config: ConfigService, private usersService: UsersService) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = context.switchToHttp().getRequest();
+        const request: Request = context.switchToHttp().getRequest();
         const token = this.extractTokenFromHeader(request);
 
         if (!token) {
             throw new UnauthorizedException();
         }
 
+        const verifyTokenOptions: JwtVerifyOptions = {
+            secret: this.config.get('JWT_SECRET'),
+            ignoreExpiration: request.url === '/v1/users/token/refresh' ? false : true
+        }
+
         try {
-            const payload = await this.jwtService.verifyAsync(token, {secret: this.config.get('JWT_SECRET')});
+            const payload = await this.jwtService.verifyAsync(token, verifyTokenOptions);
 
             const user = await this.usersService.findOneById(payload.sub);
             request['user'] = user;

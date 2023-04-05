3603409b056072264c17ceed3a72fa4bcda3258d
XiovV <xiovv@tutanota.com>
2023-04-05 15:53:50 +0200

    create post dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,11 @@
-export class CreatePostDto {}
+import { Length } from "class-validator"
+
+export class CreatePostDto {
+    userId: number
+
+    @Length(1, 5000)
+    body: string
+
+    @Length(1, 200)
+    title: string
+}

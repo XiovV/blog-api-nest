7ae5d10520c6d018aacd3080aafd2faf24a22156
XiovV <xiovv@tutanota.com>
2023-04-05 15:53:28 +0200

    implemented the endpoint for creating a new post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Version, UseGuards, ValidationPipe, Request } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtGuard } from 'src/auth/jwt.guard';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @Version('1')
+  @UseGuards(JwtGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  create(@Body(new ValidationPipe()) createPostDto: CreatePostDto, @Request() req) {
+    const user: User = req.user;
+
+    return this.postsService.create(user, createPostDto);
   }
 
   @Get()

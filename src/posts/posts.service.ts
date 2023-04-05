203b06f92b15c57cf5fc4db63d64ce1849dc07f5
XiovV <xiovv@tutanota.com>
2023-04-05 15:52:58 +0200

    implemented the create post method

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(@InjectRepository(Post) private postsRepository: Repository<Post>) {}
+
+  async create(user: User, createPostDto: CreatePostDto) {
+    const post = new Post();
+    post.body = createPostDto.body;
+    post.title = createPostDto.title;
+    post.user = user;
+
+
+    await this.postsRepository.save(post)
   }
 
   findAll() {

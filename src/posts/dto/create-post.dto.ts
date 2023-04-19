aa521debdcf4b3f16345774880095b54f66850b3
XiovV <xiovv@tutanota.com>
2023-04-19 16:36:41 +0200

    added ApiProperty decorators for swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProduces, ApiProperty } from "@nestjs/swagger"
 import { Length } from "class-validator"
 
 export class CreatePostDto {
+    @ApiProperty({minimum: 1, maximum: 5000})
     @Length(1, 5000)
     body: string
 
+    @ApiProperty({minimum: 1, maximum: 200})
     @Length(1, 200)
     title: string
 }

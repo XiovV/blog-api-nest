fa3eee087ee9ead51e76d52c4298d6ac05c95786
XiovV <xiovv@tutanota.com>
2023-04-19 15:18:24 +0200

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('NestJS Blog API V1')
+  .setDescription('Blog API is a simple real-world RESTful API, built in NestJS. It allows you to create users and posts, as well as manage them.')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

aafcf60ff5a99fd47a4bcacba23217c846dddf11
XiovV <xiovv@tutanota.com>
2023-04-19 15:19:26 +0200

    added swagger decorators for documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,11 +12,17 @@ import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { MailerService } from 'src/mailer/mailer.service';
 import { PasswordResetEmailDto } from './dto/password-reset-email.dto';
 import { PasswordResetDto } from './dto/password-reset.dto';
+import { ApiAcceptedResponse, ApiBadRequestResponse, ApiBearerAuth, ApiConflictResponse, ApiCreatedResponse, ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiQuery, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { BadRequestError, ConflictError, DefaultUnauthorizedError, ErrorResponse, ForbiddenError, NotFoundError, SetupMFAResponse, TokenPair, UnauthorizedError } from 'src/swagger/swagger.responses';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService, private authService: AuthService, private cryptoService: CryptoService, private mailerService: MailerService) { }
 
+  @ApiOperation({ summary: "Registers a user into the system.", description: "Inserts a user into the database if the username or email haven't already been taken." })
+  @ApiCreatedResponse({ description: 'User has been successfully created', type: TokenPair })
+  @ApiConflictResponse({ description: 'Username or email already exists', type: ConflictError })
   @Version('1')
   @Post('register')
   async create(@Body(new ValidationPipe()) createUserDto: CreateUserDto) {
@@ -25,12 +31,26 @@ export class UsersController {
     return await this.authService.generateTokenPair(createdUser);
   }
 
+  @ApiOperation({
+    summary: "Returns an access token if the credentials are correct.",
+    description: "Access token and refresh token are returned if the username and password are correct. The totp field must be provided if the user has 2FA enabled. If not, it can be completely left out."
+  })
+  @ApiOkResponse({ description: 'User successfully logged in', type: TokenPair })
+  @ApiUnauthorizedResponse({ description: 'Username, password or totp code is incorrect', type: UnauthorizedError })
+  @ApiForbiddenResponse({ description: 'The user has 2FA enabled, but the totp code was not provided', type: ForbiddenError })
   @Version('1')
   @Post('login')
   async login(@Body(new ValidationPipe()) loginUserDto: LoginUserDto) {
     return this.authService.login(loginUserDto.username, loginUserDto.password, loginUserDto.totp);
   }
 
+
+  @ApiOperation({
+    summary: "Login via a recovery code.",
+    description: "Access token and refresh token are returned if the username, password and recovery code are correct. This endpoint can only be used if the user has 2FA enabled but has lost access to the TOTP secret."
+  })
+  @ApiOkResponse({ description: "User successfully logged in", type: TokenPair })
+  @ApiUnauthorizedResponse({ description: 'Username, password or recovery code is incorrect', type: UnauthorizedError })
   @Version('1')
   @Post('login/recovery')
   async loginRecovery(@Body(new ValidationPipe()) loginUserRecoveryDto: LoginUserRecoveryDto) {
@@ -41,6 +61,10 @@ export class UsersController {
     return await this.authService.generateTokenPair(user);
   }
 
+  @ApiOperation({ summary: "Generates a TOTP secret." })
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'TOTP code generated successfully', type: SetupMFAResponse })
+  @ApiUnauthorizedResponse({ description: 'The access token is invalid', type: DefaultUnauthorizedError })
   @Version('1')
   @UseGuards(JwtGuard)
   @Get('mfa')
@@ -50,9 +74,13 @@ export class UsersController {
     return { secret }
   }
 
+  @ApiOperation({ summary: "Checks if provided totp code is correct and enables 2FA for the user." })
+  @ApiBearerAuth()
+  @ApiBadRequestResponse({ description: 'The totp code is incorrect', type: BadRequestError })
+  @ApiUnauthorizedResponse({ description: 'The access token is invalid', type: DefaultUnauthorizedError })
   @Version('1')
   @UseGuards(JwtGuard)
-  @Post('mfa/confirm')
+  @Put('mfa/confirm')
   async confirmMfa(@Body(new ValidationPipe()) confirmMfaDto: ConfirmMfaDto, @Request() req) {
     if (!this.authService.verifyTOTPCode(confirmMfaDto.totp, confirmMfaDto.secret)) {
       throw new HttpException('the provided totp is invalid', HttpStatus.BAD_REQUEST);
@@ -67,7 +95,15 @@ export class UsersController {
     return recoveryCodes;
   }
 
+  @ApiOperation({
+    summary: "Returns a fresh token pair.",
+    description: "Used only when the access token has expired. If a previously used refresh token is used again, the user's account will be locked for security reasons."
+  })
+  @ApiOkResponse({ description: "Tokens have been refreshed successfully", type: TokenPair })
+  @ApiUnauthorizedResponse({description: "The access token or refresh token is invalid", type: DefaultUnauthorizedError})
+  @ApiBearerAuth()
   @Version('1')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(JwtGuard)
   @Post('token/refresh')
   async refreshToken(@Body(new ValidationPipe()) refreshTokenDto: RefreshTokenDto, @Request() req) {
@@ -92,7 +128,12 @@ export class UsersController {
     return await this.authService.generateTokenPair(user);
   }
 
+
+  @ApiOperation({summary: "Sends a password reset email."})
+  @ApiAcceptedResponse({description: "Email has been sent"})
+  @ApiNotFoundResponse({description: "The provided email does not exist", type: NotFoundError})
   @Version('1')
+  @HttpCode(HttpStatus.ACCEPTED)
   @Post('password-reset')
   async sendPasswordResetMail(@Body(new ValidationPipe()) passwordResetEmailDto: PasswordResetEmailDto) {
     const user = await this.usersService.findOneByEmail(passwordResetEmailDto.email)
@@ -107,7 +148,12 @@ export class UsersController {
     this.mailerService.sendPasswordResetMail(user.email, user.username, passwordResetToken);
   }
 
+  @ApiOperation({summary: "Changes the user's password."})
+  @ApiQuery({name: "password reset token"})
+  @ApiOkResponse({description: "Password has been changed successfully"})
+  @ApiBadRequestResponse({description: 'The password reset token was not provided', type: BadRequestError})
   @Version('1')
+  @HttpCode(HttpStatus.OK)
   @Put('password-reset')
   async resetUserPassword(@Body(new ValidationPipe()) passwordResetDto: PasswordResetDto, @Query() query) {
     const { token } = query;
@@ -115,6 +161,7 @@ export class UsersController {
       throw new HttpException('please provide a password reset token', HttpStatus.BAD_REQUEST);
     }
 
+    //TODO: return a response according to the docs
     await this.usersService.resetUserPasswordByResetToken(passwordResetDto.password, token);
   }
 }

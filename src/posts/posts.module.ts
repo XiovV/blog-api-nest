1c8e700de2c0b2c9853bae1d671660b618e03582
XiovV <xiovv@tutanota.com>
2023-04-22 13:17:07 +0200

    imported Casbin

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -9,10 +9,11 @@ import { UsersService } from 'src/users/users.service';
 import { User } from 'src/users/entities/user.entity';
 import { BlacklistedToken } from 'src/users/entities/token-blacklist.entity';
 import { PasswordResetToken } from 'src/users/entities/password-reset-token.entity';
+import { Casbin } from '../casbin/casbin';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post, User, BlacklistedToken, PasswordResetToken])],
   controllers: [PostsController],
-  providers: [PostsService, ConfigService, UsersService, JwtService],
+  providers: [PostsService, ConfigService, UsersService, JwtService, Casbin],
 })
 export class PostsModule {}

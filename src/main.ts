65405fc220b4a8320d87e3e79eafd6842a3cc3b3
XiovV <xiovv@tutanota.com>
2023-05-16 16:04:03 +0200

    using winston as the default logger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger } from 'nestjs-pino';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true});
-  app.useLogger(app.get(Logger))
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
   app.enableVersioning({
     type: VersioningType.URI,
   });
